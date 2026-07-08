527426463815ad703290affc37ed43361b558876
rayanyahiaoui13 <rayan1.yahiaoui@gmail.com>
2026-07-08 14:50:56 +0800

    fixing error

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 export const MQTT_CONFIG = {
   //broker: "192.168.0.133",
   //broker: "100.69.184.106", //tailscale
-  broker: "10.69.85.576", //hotspot
+  broker: "10.69.85.57", //hotspot
   port: 9001,
   clientId: "DashboardWebRTC_" + Math.random().toString(16).substr(2, 8),
 };
