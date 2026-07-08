a8c966b2ebd40e31cca65e7f5fbc01f1c60b0387
rayanyahiaoui13 <rayan1.yahiaoui@gmail.com>
2026-07-08 15:10:02 +0800

    localip

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // js/config.js
 export const MQTT_CONFIG = {
-  //broker: "192.168.0.133",
+  broker: "192.168.0.133",
   //broker: "100.69.184.106", //tailscale
-  broker: "10.69.85.57", //hotspot
+  //broker: "10.69.85.57", //hotspot
   port: 9001,
   clientId: "DashboardWebRTC_" + Math.random().toString(16).substr(2, 8),
 };
